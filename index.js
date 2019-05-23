78fdb7891100ae64154d36f1ea4ab94a5c811ce9
kimwoongkyu <darkgantz@naver.com>
2019-05-23 10:53:21 +0900

    replace React.createClass with CreateReactClass

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 var React = require('react');
 var ReactNative = require('react-native');
+var CreateReactClass = require('create-react-class');
 
 var {
     Text,
@@ -8,7 +9,7 @@ var {
 
 import code128 from "./code128"
 
-export default React.createClass({
+export default CreateReactClass({
     render() {
         const {text, style, ...others } = this.props
         return <Text {...others} style= { [{ color: 'black', fontSize:50, fontFamily: 'code128' }, style, ]} >{ code128(text) }</Text >
